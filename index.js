e1ffd30fc677bfc9a3c2cf06d08a08f2658a40e3
davialexandreh <d19alexandre@hotmail.com>
2024-02-26 10:54:16 -0300

    Finalização do meu primeiro projeto em node.js

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const uuid = require('uuid')
+
+const app = express()
+app.use(express.json())
+
+const usuarios = []
+
+const checkIdUsuario = (request, response, next) => {
+    const { id } = request.params
+    const index = usuarios.findIndex(usuario => usuario.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ Error: "❌ Usuário não cadastrado!" })
+    }
+
+    request.usuarioIndex = index
+    request.usuarioId = id
+
+    next()
+}
+
+app.get('/usuarios', (request, response) => {
+    return response.send(usuarios)
+})
+
+app.post('/usuarios', (request, response) => {
+    const { nome, idade } = request.body
+    const usuario = { id: uuid.v4(), nome, idade }
+
+    usuarios.push(usuario)
+    return response.status(201).send(usuario)
+})
+
+app.put('/usuarios/:id', checkIdUsuario, (request, response) => {
+    const { nome, idade } = request.body
+    const index = request.usuarioIndex
+    const id = request.usuarioId
+
+    const atualizacaoUsuario = { id, nome, idade }
+
+    usuarios[index] = atualizacaoUsuario
+
+    return response.send(atualizacaoUsuario)
+})
+
+app.delete('/usuarios/:id', checkIdUsuario, (request, response) => {
+    const index = request.usuarioIndex
+
+    usuarios.splice(index, 1)
+
+    return response.status(204).json()
+})
+
+app.listen(3000, () => {
+    console.log("✅ Servidor online")
+})
